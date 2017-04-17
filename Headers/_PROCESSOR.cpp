2cf31a0f86b2ec9f943aab95547c1ab4eb3e8000
methoz <aligholami7596@gmail.com>
2017-04-17 17:27:42 +0430

    Some good amount of processor attributes are now here

--- Headers/_PROCESSOR.cpp
+++ Headers/_PROCESSOR.cpp
@@ -3,6 +3,8 @@
 
 class Processor{
 private:
+  float processorCacheSize;   // The cache size in the processor
+  float processSize;    // Specified in nanometers (nm), and defines the size of the smallest individual elements on a processor die
   float processorClockSpeed;    // The clock speed for the processor
   String processorName;   // A name for our processor :D
   Scheduler *scheduler;   // The scheduler which is used in the processor
