79a03d9994dec72d2579cd5b4d734e69ce2af2a9
methoz <aligholami7596@gmail.com>
2017-04-17 16:58:30 +0430

    add process header file + define some variables

--- Headers/_PROCESS.cpp
+++ Headers/_PROCESS.cpp
@@ -1,7 +1,13 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-  cout << "Hello World :D !\n";
-  return 0;
+#ifndef __PROCESS_CPP
+#define __PROCESS_CPP
+
+class process {
+
+  int pID;    // Process ID
+  int numOfCycles;    // Number of cycles that each process is being executed on the cpu
+  int timeEntered;    // The time that it has entered to the system
+
+  // The need of using each processor is stated using an array which is a type of the PROCESSOR
+  processor *processorTypes;    // This will be allocated next in the code :D
+
 }
