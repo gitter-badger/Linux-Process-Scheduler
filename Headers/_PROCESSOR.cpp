bc45729b469c5d3fedbed460769f39375a9897f2
methoz <aligholami7596@gmail.com>
2017-04-17 17:25:03 +0430

    add other attributes of a processor

--- Headers/_PROCESSOR.cpp
+++ Headers/_PROCESSOR.cpp
@@ -3,7 +3,11 @@
 
 class Processor{
 private:
-  Scheduler *scheduler;
-  
+  float processorClockSpeed;    // The clock speed for the processor
+  String processorName;   // A name for our processor :D
+  Scheduler *scheduler;   // The scheduler which is used in the processor
 
+
+public:
+  virtual ~Processor();   // Virtual class constructor
 }
