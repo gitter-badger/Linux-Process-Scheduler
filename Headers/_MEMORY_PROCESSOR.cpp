6d082323401ee348e6591411e9f11047f2c0dbfd
methoz <aligholami7596@gmail.com>
2017-04-18 23:27:10 +0430

    add the superclass call for the inherited class

--- Headers/_MEMORY_PROCESSOR.cpp
+++ Headers/_MEMORY_PROCESSOR.cpp
@@ -5,5 +5,8 @@
 /* Inherit from the Processor class */
 class MemoryProcessor : public Process
 {
-  
+public:
+  MemoryProcessor(float processorCacheSize, float processSize, float processorClockSpeed, string processorName, Scheduler *scheduler):
+  /* Call the superclass(Processor) constructor */
+  Processor(processorCacheSize, processSize, processorClockSpeed, processorName, scheduler)
 }
