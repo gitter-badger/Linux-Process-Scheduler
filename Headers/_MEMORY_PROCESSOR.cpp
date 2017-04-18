63e05ac4859cb70f1f733ad2be5914c4dce3f4ee
methoz <aligholami7596@gmail.com>
2017-04-18 23:15:06 +0430

    adding the memory processor class

---
+++ Headers/_MEMORY_PROCESSOR.cpp
@@ -0,0 +1,9 @@
+#ifndef __MEMORY_PROCESSOR_CPP_
+#define __MEMORY_PROCESSOR_CPP_
+#include <__PROCESS_CPP_>
+
+/* Inherit from the Processor class */
+class MemoryProcessor : public Process
+{
+  
+}
