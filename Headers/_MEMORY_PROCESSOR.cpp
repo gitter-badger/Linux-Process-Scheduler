d1973cb3e895d8e7370b4f780b02954b448df0cf
methoz <aligholami7596@gmail.com>
2017-04-27 23:52:38 +0430

    fix inheritence

--- Headers/_MEMORY_PROCESSOR.cpp
+++ Headers/_MEMORY_PROCESSOR.cpp
@@ -3,7 +3,7 @@
 #include <__PROCESS_CPP_>
 
 /* Inherit from the Processor class */
-class MemoryProcessor : public Process
+class MemoryProcessor : public Processor
 {
 public:
   MemoryProcessor(float processorCacheSize, float processSize, float processorClockSpeed, string processorName, Scheduler *scheduler)
